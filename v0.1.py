8e3a0e2a4b67a71c716a733f2200b5a3e59e505a
AdamsMao <464048025@qq.com>
2015-09-04 17:56:23 +0800

    9-4

--- v0.1.py
+++ v0.1.py
@@ -7,57 +7,69 @@ import json
 
 from collections import deque
  
-f = open('data.json','wb')
-
-queue = deque()
-visited = set()
- 
-#url = 'http://news.dbanotes.net/'  # 入口页面, 可以换成别的
-url = 'http://news.baidu.com/'  # 入口页面, 可以换成别的
-char_type = 'utf-8'
- 
-queue.append(url)
-cnt = 0
- 
-while queue:
-	url = queue.popleft()  # 队首元素出队
-  	visited |= {url}  # 标记为已访问
- 
-  	print('已经抓取: ' + str(cnt) + '   正在抓取 <---  ' + url)
-  	cnt += 1
-	
+def pipeline(html,f):
 	try:
-  		urlop = urllib2.urlopen(url,timeout=6)
-	except KeyboardInterrupt:
-		break
- 	except:
-		print '超时 ---> '
-		continue
-
-  	# 避免程序异常中止, 用try..catch处理异常
-  	try:
-    		data = urlop.read()
-		# Find out Web charset type
-		char_match = re.search(r'.*?charset=(.*?)[\'\"].*?',data)
-		if char_match:
-			char_type = char_match.group(1) 
-		data = data.decode(char_type).encode('utf-8')
 		link_re = re.compile(r'<a.*?href=[\"\']([http://|https://].*?)[\"\'].*?>(.*?)</a>')
-		link_list = link_re.findall(data)
+		link_list = link_re.findall(html)
 		for i in link_list:
 			f.write(i[0])
 			f.write('\n')
 			f.write(i[1])
 			f.write('\n\n')
   	except:
-		print 'Unexpected exception.'
+		print 'Unexpected pipeline exception.'
     		#continue
- 
-  	# 正则表达式提取页面中所有队列, 并判断是否已经访问过, 然后加入待爬队列
-  	linkre = re.compile(r'<a.*?href=[\"\']([http://|https://].*?)[\"\'].*?>.*?</a>')
-  	for x in linkre.findall(data):
-	    	if 'http' in x and x not in visited:
-      			queue.append(x)
-      			print('加入队列 --->  ' + x)
 
-f.close()
+def spider():
+	#url = 'http://news.dbanotes.net/'  # 入口页面, 可以换成别的
+	url = 'http://news.baidu.com/'  # 入口页面, 可以换成别的
+	cnt = 0
+	queue = deque()
+	queue.append(url)
+	visited = set()
+	f = open('data.json','wb')
+	char_type = 'utf-8'
+
+	while queue:
+		url = queue.popleft()  # 队首元素出队
+  		visited |= {url}  # 标记为已访问
+ 
+  		print('已经抓取: ' + str(cnt) + '   正在抓取 <---  ' + url)
+  		cnt += 1
+	
+		# Open a link, maybe meet a bad link, should rounded by try...catch
+		try:
+  			urlop = urllib2.urlopen(url,timeout=6) # timeout 6 seconds
+		except KeyboardInterrupt:  # Manual interrupt the spider
+			break
+ 		except:
+			print '超时 ---> '
+			continue
+
+  		# 避免程序异常中止, 用try..catch处理异常
+  		try:
+    			data = urlop.read()
+			# Find out Web charset type
+			char_match = re.search(r'.*?charset=(.*?)[\'\"].*?',data)
+			if char_match:
+				char_type = char_match.group(1) 
+			data = data.decode(char_type).encode('utf-8')
+			# Handle the HTML page
+			pipeline(data)
+  		except:
+			print 'Unexpected read() exception.'
+    			#continue
+ 		
+		# 正则表达式提取页面中所有队列, 并判断是否已经访问过, 然后加入待爬队列
+  		linkre = re.compile(r'<a.*?href=[\"\']([http://|https://].*?)[\"\'].*?>.*?</a>')
+		links = linkre.findall(data)
+  		for x in links:
+	    		if 'http' in x and x not in visited:
+      				queue.append(x)
+      				print('加入队列 --->  ' + x)
+				
+	f.close()
+
+if __name__ == "__main__":
+	spider()
+
