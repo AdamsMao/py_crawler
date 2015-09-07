11619b98070c8c9182e9a5a2d8efcb8a8864e5ea
AdamsMao <464048025@qq.com>
2015-09-07 17:57:43 +0800

    9-7

---
+++ test_spynner.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*'
+
+import spynner  
+      
+if __name__ == "__main__":  
+	browser = spynner.Browser()  
+        # 设置代理  
+        #browser.set_proxy('http://host:port')  
+        browser.show()  
+        try:  
+		browser.load(url='http://www.baidu.com', load_timeout=120, tries=1)  
+        except spynner.SpynnerTimeout:  
+            	print 'Timeout.'  
+        else:  
+            	# 输入搜索关键字  
+           	browser.wk_fill('input[id="kw"]', 'vba')  
+            	# 点击搜索按钮，并等待页面加载完毕  
+            	browser.wk_click('input[id="su"]', wait_load=True)  
+            	# 获取页面的HTML  
+            	html = browser.html  
+            	if html:  
+                	html = html.encode('utf-8')  
+                	open('search_results.html', 'w').write(html)  
+	browser.close() 
+
+ 
