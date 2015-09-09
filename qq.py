9d20f6bdaee614ffc3f8b2db59a256f01c509797
AdamsMao <464048025@qq.com>
2015-09-09 23:17:13 +0800

    9-10

--- qq.py
+++ qq.py
@@ -9,21 +9,22 @@ if __name__ == "__main__":
         #browser.set_proxy('http://host:port')  
         browser.show()  
         try:  
-		browser.load(url='http://www.zhihu.com', load_timeout=5, tries=1)  
+		browser.load(url='http://qzone.qq.com/', load_timeout=5, tries=1)  
         except:  
             	print 'Timeout.'  
-         
-        browser.wk_click('a[class="js-signin signin-switch with-icon"]', wait_load=True)  
-	browser.wait(3)
+        #browser.wait(5) 
+        #browser.wk_click('a[class="switch_btn_focus"]', wait_load=True)  
+	#browser.wait(3)
 	# Input username & ID	
-        browser.wk_fill('input[name="account"]', '464048025@qq.com')  
-	browser.wait(3)
-        browser.wk_fill('input[name="password"]', 'Dex156')  
-	browser.wait(3)
+        #browser.wk_fill('input[id="u"]', '464048025@qq.com')  
+	#browser.wait(3)
+        #browser.wk_fill('input[id="p"]', 'Password')  
+	#browser.wait(3)
 	# Click login
-	browser.wk_click('button[class="sign-button submit"]', wait_load=True)
+	#browser.wk_click('input[id="login_button"]', wait_load=True)
 	# Wait to input verify code manually
-	raw_input('wait to input verify code manually!')
+	wait_value = raw_input('wait to input verify code manually!')
+	browser.wait(5)
         # 获取页面的HTML  
         html = browser.html  
         if html:  
